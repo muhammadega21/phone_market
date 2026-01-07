1ff7974dad223ca385ff9dc781cfc41e8b46947a
muhammadega21 <dermawane988@gmail.com>
2026-01-07 10:19:26 +0700

    update login.tsx and register.tsx

--- resources/js/pages/auth/login.tsx
+++ resources/js/pages/auth/login.tsx
@@ -1,5 +1,27 @@
+import register from '@/routes/register';
+import { Link } from '@inertiajs/react';
+
 const Login = () => {
-    return <div>Login</div>;
+    return (
+        <div className="grid h-screen w-screen place-items-center">
+            <fieldset className="fieldset w-sm rounded-box border border-base-300 bg-white p-4">
+                <h1 className="mb-4 text-center text-xl font-bold">Login</h1>
+                <label className="label">Email</label>
+                <input type="email" className="input w-full" placeholder="Email" />
+
+                <label className="label">Password</label>
+                <input type="password" className="input w-full" placeholder="Password" />
+
+                <button className="btn mt-2 btn-primary">Login</button>
+                <p className="text-sm">
+                    Belum punya akun?{' '}
+                    <Link href={register.index.url()} className="text-sky-600 underline">
+                        Daftar
+                    </Link>
+                </p>
+            </fieldset>
+        </div>
+    );
 };
 
 export default Login;
--- resources/js/pages/auth/register.tsx
+++ resources/js/pages/auth/register.tsx
@@ -1,5 +1,30 @@
+import login from '@/routes/login';
+import { Link } from '@inertiajs/react';
+
 const Register = () => {
-    return <div>Register</div>;
+    return (
+        <div className="grid h-screen w-screen place-items-center">
+            <fieldset className="fieldset w-sm rounded-box border border-base-300 bg-white p-4">
+                <h1 className="mb-4 text-center text-xl font-bold">Register</h1>
+                <label className="label">Nama</label>
+                <input type="text" className="input w-full" placeholder="Nama" />
+
+                <label className="label">Email</label>
+                <input type="email" className="input w-full" placeholder="Email" />
+
+                <label className="label">Password</label>
+                <input type="password" className="input w-full" placeholder="Password" />
+
+                <button className="btn mt-2 btn-primary">Daftar</button>
+                <p className="text-sm">
+                    Sudah punya akun?{' '}
+                    <Link href={login.index.url()} className="text-sky-600 underline">
+                        Login
+                    </Link>
+                </p>
+            </fieldset>
+        </div>
+    );
 };
 
 export default Register;
